35664d23d734e0d744c9fd7105916bbc5d6e33a6
MATSUYAMA Kengo <macksx@gmail.com>
2009-10-09 02:49:24 +0900

    Modify spec comments.

--- spec/pastiche_spec.rb
+++ spec/pastiche_spec.rb
@@ -19,26 +19,26 @@ describe 'Pastiche' do
     Pastiche
   end
 
-  it 'returns top page' do
+  it 'returns top page at /' do
     visit '/'
     last_response.should be_ok
     last_response.body.should have_selector('title', :content => 'Pastiche')
   end
 
-  it 'returns login form' do
+  it 'returns login form at /login' do
     visit '/login'
     last_response.should be_ok
     last_response.body.should have_selector('form')
     last_response.body.should have_selector('input', :class => 'openid_identifier')
   end
 
-  it 'returns snippet' do
+  it 'returns snippet at /ID' do
     get '/12345'
     last_response.should be_ok
     last_response.body.should == ':snippet/12345'
   end
 
-  it 'returns user' do
+  it 'returns user page at /user/NAME' do
     get '/user/hoge'
     last_response.should be_ok
     last_response.body.should == ':user/hoge'
